9126f7289fbbbf86c56a9a48e82a82d6c0e52d1f
Vikas Bhardwaj <138603823+Vikas1922@users.noreply.github.com>
2024-08-09 15:42:38 +0530

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(props) {
-  return (
-    <div className="footer_sec">
-      <p>Copyright &#169; {props.currYear}</p>
-    </div>
-  );
-}
