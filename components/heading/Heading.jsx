976c510b74a484463479c3bd028fca0ee5ca25e4
Kaali rao <vikasbhardwaj013@gmail.com>
2024-08-09 15:58:38 +0530

    first commit

---
+++ components/heading/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Heading() {
+  return (
+    <div className="heading_container">
+      <h1>Keeper</h1>
+    </div>
+  );
+}
---
+++ components/note/userNotes.js
@@ -0,0 +1,34 @@
+// const notes = [
+//   {
+//     id: "1",
+//     title: "This is the title",
+//     note: "Here you write our thoughts",
+//   },
+//   {
+//     id: "2",
+//     title: "This is the title2",
+//     note: "Here you write our thoughts2",
+//   },
+//   {
+//     id: "3",
+//     title: "This is the title3",
+//     note: "Here you write our thoughts3",
+//   },
+//   {
+//     id: "4",
+//     title: "This is the title4",
+//     note: "Here you write our thoughts4",
+//   },
+//   {
+//     id: "5",
+//     title: "This is the title5",
+//     note: "Here you write our thoughts5",
+//   },
+//   {
+//     id: "6",
+//     title: "This is the title6",
+//     note: "Here you write our thoughts6",
+//   },
+// ];
+
+// export default notes;
